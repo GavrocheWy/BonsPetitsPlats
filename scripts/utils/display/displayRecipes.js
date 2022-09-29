14f4233f056d8646b0ccdb55d0533fb7c5cd9606
Gavroche Woerly <gavrochewy@gmail.com>
2022-09-29 15:04:11 +0200

    Version du premier algorithme

--- scripts/utils/display/displayRecipes.js
+++ scripts/utils/display/displayRecipes.js
@@ -4,9 +4,11 @@ const recipesSection = document.querySelector('.main-content__display')
 
 const displayRecipes = () => {
 
-    // CHECK DES TAGS
+    // TAGS + SEARCH OBLIGATOIRE
 
-    if ((!Object.values(displayedTagsParams).every((array) => array.length === 0)) || (currentSearch !== null)) {
+    if ((!Object.values(displayedTagsParams).every((array) => array.length === 0)) && (currentSearch !== null)) {
+
+        console.log('Recherche par tag + search')
 
         updatedRecipes = []
 
@@ -28,9 +30,7 @@ const displayRecipes = () => {
 
                 if (currentSearch !== null) {
 
-                    if (recipe.name.toLowerCase().includes(currentSearch) || recipe.description.includes(currentSearch) || allIngredientsNamesOfThisRecipe.includes(currentSearch)) 
-                    
-                    {
+                    if (recipe.name.toLowerCase().includes(currentSearch) || recipe.description.includes(currentSearch) || allIngredientsNamesOfThisRecipe.includes(currentSearch)) {
 
                         return true
 
@@ -130,7 +130,171 @@ const displayRecipes = () => {
 
         updateFilters()
 
-    } else {
+    }
+
+    // SEARCH UNIQUEMENT
+
+    else if ((Object.values(displayedTagsParams).every((array) => array.length === 0)) && (currentSearch !== null)) {
+
+        console.log('Recherche par search uniquement')
+
+        updatedRecipes = []
+
+        allRecipes.forEach(recipe => {
+
+            // Récupération des noms des ingrédients de la recette
+
+            let allIngredientsNamesOfThisRecipe = []
+
+            recipe.ingredients.forEach(ingredient => {
+
+                allIngredientsNamesOfThisRecipe.push(ingredient.ingredient.toLowerCase())
+
+            });
+
+            // Check de la recherche
+
+            function checkForSearchQuery() {
+
+                if (currentSearch !== null) {
+
+                    if (recipe.name.toLowerCase().includes(currentSearch) || recipe.description.includes(currentSearch) || allIngredientsNamesOfThisRecipe.includes(currentSearch)) {
+
+                        return true
+
+                    }
+
+                } else {
+
+                    return true
+
+                }
+
+            }
+
+            checkForSearchQuery() ? updatedRecipes.push(recipe) : null;
+
+        });
+
+        currentlyDisplayedRecipes = updatedRecipes
+
+        generateRecipesDOM()
+
+        updateFilters()
+
+    }
+
+    // TAGS UNIQUEMENT
+
+    else if ((!Object.values(displayedTagsParams).every((array) => array.length === 0)) && (currentSearch === null)) {
+
+        console.log('Recherche par tag uniquement')
+
+        updatedRecipes = []
+
+        allRecipes.forEach(recipe => {
+
+            // Récupération des noms des ingrédients de la recette
+
+            let allIngredientsNamesOfThisRecipe = []
+
+            recipe.ingredients.forEach(ingredient => {
+
+                allIngredientsNamesOfThisRecipe.push(ingredient.ingredient.toLowerCase())
+
+            });
+
+            // Check des ustensils
+
+            function checkForUstensil() {
+
+                if (displayedTagsParams.ustensils.length > 0) {
+
+                    if (displayedTagsParams.ustensils.every(ustensil => recipe.ustensils.includes(ustensil))) {
+
+                        return true
+
+                    } else {
+
+                        return false
+
+                    }
+
+                } else {
+
+                    return true
+
+                }
+
+            }
+
+            // Check des appareils
+
+            function checkForAppliance() {
+
+                if (displayedTagsParams.appliance.length > 0) {
+
+                    if (displayedTagsParams.appliance.every(appliance => recipe.appliance.toLowerCase().includes(appliance))) {
+
+                        return true
+
+                    } else {
+
+                        return false
+
+                    }
+
+                } else {
+
+                    return true
+
+                }
+
+            }
+
+            // Check des ingredients
+
+            function checkForIngredients() {
+
+                if (displayedTagsParams.ingredients.length > 0) {
+
+                    if (displayedTagsParams.ingredients.every(ingredient => allIngredientsNamesOfThisRecipe.includes(ingredient))) {
+
+                        return true
+
+                    } else {
+
+                        return false
+
+                    }
+
+                }
+
+                else {
+
+                    return true
+
+                }
+
+            }
+
+            // Check final de tous les filtres
+
+            checkForUstensil() && checkForAppliance() && checkForIngredients() ? updatedRecipes.push(recipe) : null;
+
+        });
+
+        currentlyDisplayedRecipes = updatedRecipes
+
+        generateRecipesDOM()
+
+        updateFilters()
+
+    }
+
+    else {
+
+        console.log('Aucune recherche')
 
         // AUCUN TAG
 
